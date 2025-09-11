f823066159b8673559068a8b3491676cd933e1eb
josephori <josephori30@gmail.com>
2025-09-11 17:11:42 +0300

    removed uneeded app functionality

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReportPage from "./components/ReportPage";
 import ComplianceForm from "./components/compliance-form-components/ComplianceForm";
 import { useComplianceFormStore } from "./store/complianceFormStore";
-import { useEffect } from "react";
 
 function AppContent() {
-  const navigate = useNavigate();
   const reportText = useComplianceFormStore((state) => state.reportText);
-  const resetForm = useComplianceFormStore((state) => state.resetForm);
-
-  useEffect(() => {
-    if (reportText) {
-      navigate("/report");
-    } else {
-      resetForm();
-    }
-  }, [reportText, navigate, resetForm]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
